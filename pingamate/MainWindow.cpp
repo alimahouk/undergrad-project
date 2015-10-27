33b49267a2ec1b1a7ae665bec384f70b5f0ac848
Ali Razzouk <amrazzouk@gmail.com>
2015-10-27 17:43:17 +0400

    Reset VS project type

---
+++ pingamate/MainWindow.cpp
@@ -0,0 +1,15 @@
+#include "MainWindow.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	pingamate::MainWindow form;
+	Application::Run(%form);
+}
